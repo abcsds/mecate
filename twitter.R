62662e2a45fb6b96417aceeb6fba29f73ebc654c
Alberto Barradas <abcsds@gmail.com>
2015-06-20 23:54:41 -0500

    Working twitter stream

--- twitter.R
+++ twitter.R
@@ -1,3 +1,6 @@
+require('twitteR')
+# setup oauth
 source('auth.R')
+
 # searchTwitter('mecate', geocode='40.7361,-73.9901,5km',  n=5000, retryOnRateLimit=1)
 head(searchTwitter('mecate',  n=5000, retryOnRateLimit=1))
