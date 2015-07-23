5027126f4ddedc31d106cd0ae7dc3cb7dd3236de
Alberto Barradas <abcsds@gmail.com>
2015-07-23 05:18:30 -0500

    Add Wordcloud

--- twitter.py
+++ twitter.py
@@ -1,4 +1,5 @@
 from twython import Twython
+from pywordcloud import pywordcloud
 
 TWITTER_APP_KEY = 'r5G7F451BPcsC2n4pk2hnXbDL'
 TWITTER_APP_KEY_SECRET = '6TURsBrRrn3QPFNkkj2Mzpye9KcF2OgeICktXwm1duKdFHWHtS'
@@ -14,6 +15,23 @@ search = t.search(q='#MECATE',
                   count=1000)
 
 tweets = search['statuses']
-
+text = ""
 for tweet in tweets:
-  print tweet['user']['screen_name'], '\n', tweet['text'], '\n'
+  # print tweet['user']['screen_name'], '\n', tweet['text'], '\n'
+  text +=  tweet['text']
+
+create(text,
+    outfile="output.html",
+    uppercase=False,
+    showfreq=True,
+    frequency=100,
+    removepunct = False,
+    minfont = 1.5,
+    maxfont = 6,
+    hovercolor="green",
+    showborder=False,
+    fontfamily='calibri',
+    width="1000px",
+    height="400px",
+    #colorlist = ["red","blue","yellow","black","green"]
+    )
